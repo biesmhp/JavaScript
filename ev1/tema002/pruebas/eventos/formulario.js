f15e880b23d2f83cba23bcd00127043181b85111
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-10-15 17:08:10 +0200

    mensajeLabel_completed

--- ev1/tema002/pruebas/eventos/formulario.js
+++ ev1/tema002/pruebas/eventos/formulario.js
@@ -1,30 +1,31 @@
 
 addEventListener('load',inicio,false);
 
-var value = document.getElementById('numero').value;
+
 
 function inicio(){
+
   inputForm=document.getElementById('enviar');
   inputForm.addEventListener('click',respuesta,false);
 }
 
 
 function par(value){
-
+  // Si el resto no es igual a 0 devuelve falso
   if (value%2!=0) {
-    // document.getElementById('mensaje').innerHTML=value+' es IMPAR';
-
     return false;
-  }else{
-    // document.getElementById('mensaje').innerHTML=value+' es PAR';
+  }else if(value%2==0){
+    // Si el resto es igual a 0
     return true;
   }
 }
 
 function respuesta(){
+  var value = document.getElementById('numero').value;
+  // alert(value);
   if (par(value)) {
-    document.getElementById('mensaje').innerHTML=value+' es IMPAR';
-  }else{
     document.getElementById('mensaje').innerHTML=value+' es PAR';
+  }else{
+    document.getElementById('mensaje').innerHTML=value+' es IMPAR';
   }
 }
