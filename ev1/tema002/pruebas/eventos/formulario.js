f0945baef00431fd5d116ecf0e0f7f30dc58cdc6
biesmhp <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-10-15 23:11:41 +0200

    mensajeLabel_betterComment_formulario

--- ev1/tema002/pruebas/eventos/formulario.js
+++ ev1/tema002/pruebas/eventos/formulario.js
@@ -1,15 +1,14 @@
-
+// Los comentarios pueden no ser 100% acertados
+// Prepara las funciones deseadas en la carga de la página
 addEventListener('load',inicio,false);
 
-
-
+// Se carga con el Listener y se queda a la escucha. Al clickar el elemento indicado se activa 'respuesta()'
 function inicio(){
-
   inputForm=document.getElementById('enviar');
   inputForm.addEventListener('click',respuesta,false);
 }
 
-
+// Recibiendo un parámetro numérico indica si es par o impar
 function par(value){
   // Si el resto no es igual a 0 devuelve falso
   if (value%2!=0) {
@@ -20,6 +19,7 @@ function par(value){
   }
 }
 
+// Llama a la funcion par(param), y muestra PAR o IMPAR según su resultado
 function respuesta(){
   var value = document.getElementById('numero').value;
   // alert(value);
