b2ce799f7a73abe8cac91aaba1baa79f57eafcaa
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-10-20 19:52:43 +0200

    pGam_js_003

---
+++ pGam/funciones.js
@@ -0,0 +1,15 @@
+addEventListener('load',inicio,false);
+
+
+function inicio(){
+  inputForm=document.getElementById('luchar');
+  inputForm.addEventListener('click',informacion,false);
+}
+
+function informacion(){
+
+}
+
+function mostrar(){
+
+}
