548ee888ee610803056a509f65ee8be15cbf7180
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-11-15 19:10:54 +0100

    ev1_examen2_ej3_003

--- ev1/brendan_control2/ej3.js
+++ ev1/brendan_control2/ej3.js
@@ -34,11 +34,59 @@ function ver(arr){
 
 // Parte 3 - INFORME
 function visual_estadistica(){
-  informe(tiradas);
-  console.log(tiradas);
+  // Hace una copia
+  let tiradasC = tiradas.slice();
+  // Recoge el resultado de informe()
+  let resultado = informe(tiradasC);
+
+  // Escribimos los resultados
+  vis0.value = resultado[0];
+  vis1.value = resultado[1];
+  vis2.value = resultado[2];
+  vis3.value = resultado[3];
+  vis4.value = resultado[4];
+  vis5.value = resultado[5];
 }
 
 function informe(arr){
-  
-  return [n0,n1,n2,n3,n4,n5,mas,menos];
+  let conteo = new Array(0,0,0,0,0,0);
+  for (let position in arr) {
+    switch (arr[position]) {
+      case 0:
+        conteo[0]++;
+        break;
+      case 1:
+        conteo[1]++;
+        break;
+      case 2:
+        conteo[2]++;
+        break;
+      case 3:
+        conteo[3]++;
+        break;
+      case 4:
+        conteo[4]++;
+        break;
+      case 5:
+        conteo[5]++;
+        break;
+      default:
+    }
+  }
+  // 'resultados' será el array que devuelva
+  let resultados = conteo.slice();
+
+  // WIP
+  // Obtenemos el más frecuente y el que menos
+  // Ordena el array
+  conteo.sort((a,b)=>{return a-b;});
+  console.log("Array ordenado para obtener el mayor y menor "+conteo);
+  // Recoge la primera posicion
+  let menos = conteo.slice(0,1);
+  // Recoge la última posición
+  let mas = conteo.slice(5);
+  console.log(menos+" y "+mas);
+  // WIP
+
+  return resultados;
 }
