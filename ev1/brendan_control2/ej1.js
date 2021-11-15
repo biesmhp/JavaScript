39afbfc70067ae7de8b9f54efc30860a607c42b1
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-11-15 17:28:27 +0100

    ev1_examen2_ej1_completed

---
+++ ev1/brendan_control2/ej1.js
@@ -0,0 +1,31 @@
+const numeros = [2,4,6,8,10,12,13,14,18,20,30];
+
+console.log("Array original: "+numeros);
+
+var cNumeros = numeros.slice();
+
+console.log("Array copiado: "+cNumeros);
+
+function cuadradosDeLosMenoresDe10(arr){
+  let arrFilteredm10 = arr.filter(menores10);
+  console.log("Array filtrado con los números menores de 10: "+arrFilteredm10);
+  let arrFiltered = arrFilteredm10.map(
+    function (element){
+      return element*element;
+    }
+  );
+  console.log("Resultado a devolver: "+arrFiltered);
+  return arrFiltered;
+}
+
+function menores10(numElement){
+  if (numElement<10) {
+    return numElement;
+  }
+}
+
+function mostrar(){
+  let res = cuadradosDeLosMenoresDe10(cNumeros);
+  document.getElementById('visResultado').innerHTML = res;
+  visResultado.value = res;
+}
