1af8a63f47a5dca0bff99fcdfd14dacd95179e73
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2021-12-09 19:24:59 +0100

    ev2_pruebas_ventanasEmergentes_002

--- ev2/bon/pruebas/ventana.js
+++ ev2/bon/pruebas/ventana.js
@@ -9,19 +9,39 @@ function inicio() {
 
   btnEvent = document.getElementById('btnVentanaAleatoria');
   btnEvent.addEventListener('click',ventanaAleatoria,false);
+
+  btnEvent = document.getElementById('btnVentanaTamano');
+  btnEvent.addEventListener('click',ventanaTamano,false);
+
+  btnEvent = document.getElementById('btnMoverDerecha');
+  btnEvent.addEventListener('click',moverDerecha,false);
 }
 
 function abreVentana() {
-  let ventana = window.open("", "myWindow", "width=200, height=100");
+  ventana = window.open("", "myWindow", "width=200, height=100");
   ventana.focus();
 }
 
 function cierraVentana() {
-  window.close();
+  ventana.close();
 }
 
 function ventanaAleatoria() {
-  let ventana = window.open("", "myWindow", "width=200, height=100");
+  ventana = window.open("", "myWindow", "width=200, height=100");
   ventana.moveTo(Math.random()*1000,Math.random()*1000);
+  ventana.focus();
+}
+
+function ventanaTamano() {
+  ventana = window.open("", "myWindow", "width=200, height=100");
+  // En el ancho y alto resto las dimensiones de la ventana para que quede bien centrada
+  ventana.moveTo((window.outerWidth-200)/2,(window.outerHeight-100)/2);
+  ventana.document.write();
+  ventana.document.write(`Alto: ${window.innerHeight}, Acho: ${window.innerWidth}`);
+  ventana.focus();
+}
+
+function moverDerecha() {
+  ventana.moveBy(50,0);
   ventana.focus();
 }
