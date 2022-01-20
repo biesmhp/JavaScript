4f611825e19f00e11100aa374f216acffbaec86c
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-01-20 19:26:08 +0100

    ev2_tema4_eventos_pizarra_002

--- ev2/eventos/rejillaColor/pizarra.js
+++ ev2/eventos/rejillaColor/pizarra.js
@@ -1,22 +1,36 @@
 addEventListener('load',inicio,false);
 
-function inicio() {
-  // alert("debug1");
-  btnEvent = document.getElementById('btnCrear');
-  btnEvent.addEventListener('click',crear,false);
-}
-
-function crear() {
-  rejilla = document.getElementById("zonaDibujo");
-  var lColumna = 10;
-  var lFila = 10;
-  for (var i = 0; i < lColumna; i++) {
-    // for (var j = 0; j < lFila; j++) {
-    //   let divF = document.createElement('div');
-    //   rejilla.appendChild(divF);
-    // }
-    let divC = document.createElement('div');
-    rejilla.appendChild(divC);
-    // rejilla.appendChild(div);
+  function inicio() {
+    // alert("debug1");
+    btnEvent = document.getElementById('btnCrear');
+    btnEvent.addEventListener('click',intermediate,false);
   }
-}
+
+  function intermediate() {
+    // var ancho = prompt("Ancho de la cuadricula");
+    var ancho = document.getElementById("inpAncho").value;
+    // alert(ancho);
+    genDivs(ancho);
+  }
+
+  function genDivs(v){
+    // var v = 10;
+    var e = document.getElementById("zonaDibujo"); // whatever you want to append the rows to:
+    e.innerHTML = '';
+    for(var i = 0; i < v; i++){
+      var row = document.createElement("div");
+      row.className = "row";
+      for(var x = 1; x <= v; x++){
+          var cell = document.createElement("div");
+          cell.className = "gridsquare";
+          cell.innerText = (i * v) + x;
+          cell.addEventListener('mouseover',function(){
+            this.style.backgroundColor = "red";
+          });
+          row.appendChild(cell);
+      }
+
+      e.appendChild(row);
+    }
+    // document.getElementById("zonaDibujo").innerText = e.innerHTML;
+  }
