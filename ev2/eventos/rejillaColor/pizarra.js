97212cd7067373a8d204fb8af02b6d310382a611
biesmhp <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-01-21 13:20:36 +0100

    ev2_tema4_eventos_pizarra_003

--- ev2/eventos/rejillaColor/pizarra.js
+++ ev2/eventos/rejillaColor/pizarra.js
@@ -1,36 +1,95 @@
 addEventListener('load',inicio,false);
 
   function inicio() {
-    // alert("debug1");
+    // Tu area de dibujo
+    areaDibujo = document.getElementById("zonaDibujo");
+
+    // Eventos
     btnEvent = document.getElementById('btnCrear');
     btnEvent.addEventListener('click',intermediate,false);
+    btnEvent = document.getElementById("inpRejilla");
+    btnEvent.addEventListener("click",rejillar,false);
+    btnEvent = document.getElementById("btnBorrar");
+    btnEvent.addEventListener("click",borrado,false);
   }
 
   function intermediate() {
-    // var ancho = prompt("Ancho de la cuadricula");
-    var ancho = document.getElementById("inpAncho").value;
-    // alert(ancho);
-    genDivs(ancho);
+    let ancho = document.getElementById("inpAncho").value; // Numero de casillas
+    crearCeldas(ancho);
   }
 
-  function genDivs(v){
-    // var v = 10;
-    var e = document.getElementById("zonaDibujo"); // whatever you want to append the rows to:
-    e.innerHTML = '';
-    for(var i = 0; i < v; i++){
-      var row = document.createElement("div");
-      row.className = "row";
-      for(var x = 1; x <= v; x++){
-          var cell = document.createElement("div");
-          cell.className = "gridsquare";
-          cell.innerText = (i * v) + x;
-          cell.addEventListener('mouseover',function(){
-            this.style.backgroundColor = "red";
-          });
-          row.appendChild(cell);
-      }
+  function crearCeldas(nCeldas) {
+    document.getElementById("zonaDibujo").innerHTML = '';
+    for (let i = 0; i < nCeldas; i++) {
+        for (let j = 0; j < nCeldas; j++) {
+          // Creamos las celdas
+          let celda = document.createElement("div");
+          // Con esto le damos una clase de nombre "celda"
+          celda.className = "celda";
+          // Las dimensiones de las celdas se generarán de forma dinámica en función del tamaño del #zonaDibujo definido como areaDibujo en inicio()
+          // Si el modo rejilla está activado se crean las casillas con borde y el tamaño reducido para que entre el borde, sino se crean las casillas con 1px de margen
+          let rejilla = document.getElementById("inpRejilla").checked;
+          if (rejilla) {
+            celda.style.borderStyle = "solid";
+            celda.style.width = `${(areaDibujo.clientWidth - 2*nCeldas - 1) / nCeldas}px`;
+            celda.style.height = `${(areaDibujo.clientHeight - 2*nCeldas - 1) / nCeldas}px`;
+          } else {
+            celda.style.width = `${(areaDibujo.clientWidth - 1) / nCeldas}px`;
+            celda.style.height = `${(areaDibujo.clientHeight - 1) / nCeldas}px`;
+          }
 
-      e.appendChild(row);
+          // Mostrar el número de celda
+          // celda.innerText = (i * nCeldas) + j;
+
+          // Añadimos eventos
+          // Pintar con clicks
+          celda.addEventListener("click", pintar, false);
+          // Pintar al mantener pulsado
+          areaDibujo.addEventListener("mousedown", function () {
+              celda.addEventListener("mouseover", pintar, false);
+          }, false);
+          // Dejar de pintar al dejar de pulsar
+          areaDibujo.addEventListener("mouseup", function () {
+              celda.removeEventListener("mouseover", pintar);
+          }, false);
+
+          document.getElementById("zonaDibujo").appendChild(celda);
+      }
     }
-    // document.getElementById("zonaDibujo").innerText = e.innerHTML;
+  }
+
+  function pintar() {
+    if (!document.getElementById("inpMode").checked) {
+      this.style.backgroundColor = document.getElementById("inpColor").value;
+    } else {
+      this.style.backgroundColor = "white";
+    }
+
+  }
+
+  function rejillar() {
+    let ancho = document.getElementById("inpAncho").value; // Numero de casillas
+    let celdas = document.getElementsByClassName("celda");
+    // A la vez que añade el borde modifica las dimensiones de la celda para que quepa con el nuevo borde o elimine el espacio extra si quita el borde
+    let rejilla = document.getElementById("inpRejilla").checked;
+    if (rejilla) {
+      for (var i = 0; i < celdas.length; i++) {
+        celdas.item(i).style.borderStyle = "solid";
+        celdas.item(i).style.width = `${(areaDibujo.clientWidth - 2*ancho - 1) / ancho}px`;
+        celdas.item(i).style.height = `${(areaDibujo.clientHeight - 2*ancho - 1) / ancho}px`;
+      }
+    } else {
+      for (var i = 0; i < celdas.length; i++) {
+        celdas.item(i).style.borderStyle = "none";
+        celdas.item(i).style.width = `${(areaDibujo.clientWidth - 1) / ancho}px`;
+        celdas.item(i).style.height = `${(areaDibujo.clientHeight - 1) / ancho}px`;
+      }
+    }
+  }
+
+  function borrado() {
+    let celdas = document.getElementsByClassName("celda");
+    for (var i = 0; i < celdas.length; i++) {
+      celdas.item(i).style.backgroundColor = "white";
+    }
   }
