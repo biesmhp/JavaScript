18754fdc79076f8cbb0c35f9c186d5d0adf5fe9f
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-04-04 19:08:46 +0200

    recuperacion_h2_ej3_completed

--- ev_recuperacion/tema1/hoja2/ej1/js.js
+++ ev_recuperacion/tema1/hoja2/ej1/js.js
@@ -1,11 +1,94 @@
 addEventListener('load',inicio,false)
 function inicio() {
+  // Creamos un número aleatorio entre 3 y 6
+  let randomNum = Math.floor(Math.random()*4+3)
+  // Creamos las dos matrices
+  let maz1 = matriz(randomNum)
+  let maz2 = matriz(randomNum)
+
+  // Evento para mostrar
   evento = document.querySelector("#btnMostrar")
   evento.addEventListener('click',function (e) {
-    mostrar(Math.floor(Math.random()*4+3))
+    // Las visualizamos
+    mostrar(maz1,'#visualizado1')
+    mostrar(maz2,'#visualizado2')
+    // Mostramos el resto de botones
+    document.querySelector("#btnSumar").hidden = false
+    document.querySelector("#btnRestar").hidden = false
+  })
+
+  // Evento de sumado
+  evento = document.querySelector("#btnSumar")
+  evento.addEventListener('click',function (e) {
+    document.querySelector("#operador").innerText = `+`
+    mostrar(mazSum(maz1,maz2),'#resultado1','sum');
+  })
+
+  // Evento de restado
+  evento = document.querySelector("#btnRestar")
+  evento.addEventListener('click',function (e) {
+    document.querySelector("#operador").innerText = `-`
+    mostrar(mazRes(maz1,maz2),'#resultado1','res');
   })
 }
 
-function mostrar(texto) {
-  document.querySelector("#visualizado").innerHTML = texto;
+// Suma de matrices
+function mazSum(maz1,maz2) {
+  let matrizSumada = []
+  for (var i = 0; i < maz1.length; i++) {
+    let arrInt = []
+    for (var y = 0; y < maz1.length; y++) {
+      arrInt.push(maz1[i][y]+maz2[i][y])
+    }
+    matrizSumada.push(arrInt)
+  }
+  return matrizSumada
+}
+
+// Resta de matrices
+function mazRes(maz1,maz2) {
+  let matrizRestada = []
+  for (var i = 0; i < maz1.length; i++) {
+    let arrInt = []
+    for (var y = 0; y < maz1.length; y++) {
+      arrInt.push(maz1[i][y]-maz2[i][y])
+    }
+    matrizRestada.push(arrInt)
+  }
+  return matrizRestada
+}
+
+// Creación de matriz
+function matriz(num) {
+  // Array multidimensional (matriz)
+  let arr = []
+  // Lo recorremos
+  for (let i = 0; i < num; i++) {
+    // Creamos el array interior
+    let arrInt = []
+    // Lo recorremos y lo rellenamos con números aleatorios
+    for (let y = 0; y < num; y++) {
+      arrInt.push(Math.floor(Math.random()*10+1))
+    }
+    // Añadimos el array interior a una nueva fila del array multidimensional
+    arr.push(arrInt)
+  }
+  // Devolvemos el array multidimensional
+  return arr
+}
+
+function mostrar(texto,objetivo,sumOres) {
+  let vis = ``
+  if (sumOres=='sum') {
+    vis += `Sumado \n`
+  }else if (sumOres=='res'){
+    vis += `Restado \n`
+  }
+  if (Array.isArray(texto)) {
+    for (var i = 0; i < texto.length; i++) {
+      vis += `${texto[i]} \n`
+    }
+  }
+  document.querySelector(objetivo).innerText = vis;
+  console.log(texto);
 }
---
+++ ev_recuperacion/tema1/hoja2/ej3_DAW226/js.js
@@ -0,0 +1,117 @@
+addEventListener('load',inicio,false)
+function inicio() {
+  // Creamos las salas
+  let salas = matriz(5)
+  let num = 0
+  // Cargamos las opciones iniciales
+  cargarSalas(salas)
+  cargarEquipos(salas,num)
+
+  // Evento para mostrar los equipos disponibles
+  evento = document.querySelector("#selSala")
+  evento.addEventListener('change',function (e) {
+    cargarEquipos(salas,e.target.selectedIndex)
+  })
+
+  // Evento para solicitar el equipo
+  evento = document.querySelector("#btnSolicitar")
+  evento.addEventListener('click',function (e) {
+    let sala = document.querySelector("#selSala").value
+    let equipo = document.querySelector("#selEquipo").value
+    if (salas[sala][equipo]) {
+      salas[sala][equipo] = false
+      cargarSalas(salas)
+      cargarEquipos(salas,sala)
+      let ocupados = parseInt(document.querySelector("#visEquiposOcupados").innerText)
+      document.querySelector("#visEquiposOcupados").innerText = ocupados+1
+      mostrar(salas,'#visualizado')
+    }else{
+      alert("Ese equipo está ocupado")
+    }
+  })
+
+  // Evento para cancelar la solicitud
+  evento = document.querySelector("#btnCancelar")
+  evento.addEventListener('click',function (e) {
+    let sala = document.querySelector("#selSala").value
+    let equipo = document.querySelector("#selEquipo").value
+    if (!(salas[sala][equipo])) {
+      salas[sala][equipo] = true
+      cargarSalas(salas)
+      cargarEquipos(salas,sala)
+      let ocupados = parseInt(document.querySelector("#visEquiposOcupados").innerText)
+      document.querySelector("#visEquiposOcupados").innerText = ocupados-1
+      mostrar(salas,'#visualizado')
+    }else{
+      alert("Ese equipo no está ocupado")
+    }
+  })
+}
+
+// Generamos las OPTION del SELECT de salas
+function cargarSalas(matriz) {
+  let opciones = ``
+  for (var i = 0; i < matriz.length; i++) {
+    let count = 0
+    for (var y = 0; y < matriz[i].length; y++) {
+      if (matriz[i][y]) {
+        count++
+      }
+    }
+    opciones += `<option value=${i}>Sala${i+1} (${count})</option>`
+  }
+  // Escribe las opciones en el SELECT de salas
+  document.querySelector("#selSala").innerHTML = opciones
+}
+
+// Generamos las OPTION del SELECT de equipos
+function cargarEquipos(matriz,sala) {
+  let opciones = ``
+  for (var i = 0; i < matriz[sala].length; i++) {
+    if (matriz[sala][i]) {
+      opciones += `<option value=${i}>Equipo${i+1}</option>`
+    }else{
+      opciones += `<option value=${i}>Equipo${i+1}--Ocupado</option>`
+    }
+  }
+  // Escribe las opciones en el SELECT de salas
+  document.querySelector("#selEquipo").innerHTML = opciones
+}
+
+function mostrar(arr,objetivo) {
+  let vis = ``
+  if (Array.isArray(arr)) {
+    for (var i = 0; i < arr.length; i++) {
+      vis += `Sala ${i+1}: `
+      for (var y = 0; y < arr[i].length; y++) {
+        if (arr[i][y]) {
+          vis += `🖥`
+        }else{
+          vis += `🖱`
+        }
+      }
+      vis += `\n\n `
+    }
+  }
+  document.querySelector(objetivo).innerText = vis;
+  console.log(arr);
+}
+
+// Creación de matriz
+function matriz(num) {
+  // Array multidimensional (matriz)
+  let arr = []
+  // Lo recorremos
+  for (let i = 0; i < num; i++) {
+    // Creamos el array interior
+    let arrInt = []
+    // Lo recorremos y lo rellenamos con números aleatorios
+    for (let y = 0; y < 20; y++) {
+      arrInt.push(true)
+    }
+    // Añadimos el array interior a una nueva fila del array multidimensional
+    arr.push(arrInt)
+  }
+  // Devolvemos el array multidimensional
+  return arr
+}
