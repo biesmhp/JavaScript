01d0f7ce13478bb4bda78db50f70bf96d3b26a07
Brendan <brendan.rodriguezfranco@iesmiguelherrero.com>
2022-06-06 19:26:01 +0200

    recuperacion_h7_ej5_v2

--- ev_recuperacion/repaso/hoja7/ej5/ej5.js
+++ ev_recuperacion/repaso/hoja7/ej5/ej5.js
@@ -3,16 +3,94 @@ addEventListener('load',inicio,false)
 
 function inicio() {
   let arrJaulas = rellenarJaulas(15)
-  // console.log(arrJaulas);
+
+  // Crear un select con las jaulas
   let selNode = document.querySelector("#selJaulas")
   addSelect(arrJaulas,selNode)
+  // WIP
+  // Crear un radio con las jaulas (WIP: e imagenes)
+  let radioNode = document.querySelector("#cajaJaulas")
+  // addRadio(arrJaulas,radioNode)
 
   let evento = document.querySelector("#btnEnviar")
   evento.addEventListener('click',function () {
     let jaula = document.querySelector("#selJaulas").value
-
     mostrar(arrJaulas[jaula],"#visualizado")
+    document.querySelector("#error").innerHTML = ""
+
+    // Creamos los botones para usar los métodos de la jaula
+    controlDeJaula(arrJaulas,jaula)
   },false)
+}
+
+function controlDeJaula(arr,jaula) {
+  let cajaEspecie = document.querySelector("#cajaEspecie")
+  cajaEspecie.innerHTML = ""
+  let cajaAnimales = document.querySelector("#cajaAnimales")
+  cajaAnimales.innerHTML = ""
+
+  // Input de la especie
+  let nodeInpEspecie = document.createElement("input")
+  nodeInpEspecie.setAttribute("id","inpEspecie")
+  nodeInpEspecie.setAttribute("type","text")
+  nodeInpEspecie.setAttribute("placeholder","Especie")
+  cajaEspecie.appendChild(nodeInpEspecie)
+
+  // Botón para elegir la especie
+  let nodeBtnElegir = document.createElement("input")
+  nodeBtnElegir.setAttribute("id","btnElegir")
+  nodeBtnElegir.setAttribute("type","button")
+  nodeBtnElegir.setAttribute("value","Marcar")
+  nodeBtnElegir.addEventListener('click',function () {
+    if (!arr[jaula].especieJaula(nodeInpEspecie.value)) {
+      mostrar("La jaula debe estar vacia","#error")
+    } else {
+      mostrar(arr[jaula],"#visualizado")
+    }
+  })
+  cajaEspecie.appendChild(nodeBtnElegir)
+
+  // Input del número de animales
+  let nodeInpNumAnimales = document.createElement("input")
+  nodeInpNumAnimales.setAttribute("id","inpNumAnimales")
+  nodeInpNumAnimales.setAttribute("type","text")
+  nodeInpNumAnimales.setAttribute("placeholder","Número de animales")
+  nodeInpNumAnimales.addEventListener('keypress',function(e) {
+    soloNumeros(e)
+  })
+  cajaAnimales.appendChild(nodeInpNumAnimales)
+
+  // Botón para meter animales
+  let nodeBtnMeter = document.createElement("input")
+  nodeBtnMeter.setAttribute("id","btnMeter")
+  nodeBtnMeter.setAttribute("type","button")
+  nodeBtnMeter.setAttribute("value","Meter")
+  nodeBtnMeter.addEventListener('click',function () {
+    if (arr[jaula].especie == "") {
+      mostrar("Debes seleccionar el tipo de especie primero","#error")
+    }else if (!arr[jaula].meterAnimales(nodeInpNumAnimales.value)) {
+      mostrar("No caben tantos animales en la jaula","#error")
+    } else {
+      mostrar(arr[jaula],"#visualizado")
+    }
+  })
+  cajaAnimales.appendChild(nodeBtnMeter)
+
+  // Botón para sacar animales
+  let nodeBtnSacar = document.createElement("input")
+  nodeBtnSacar.setAttribute("id","btnSacar")
+  nodeBtnSacar.setAttribute("type","button")
+  nodeBtnSacar.setAttribute("value","Sacar")
+  nodeBtnSacar.addEventListener('click',function () {
+    if (!arr[jaula].sacarAnimales(nodeInpNumAnimales.value)) {
+      mostrar("No hay tantos animales en la jaula","#error")
+    } else {
+      mostrar(arr[jaula],"#visualizado")
+    }
+  })
+  cajaAnimales.appendChild(nodeBtnSacar)
+
+  mostrar(arr[jaula],"#visualizado")
 }
 
 function rellenarJaulas(num) {
@@ -24,6 +102,17 @@ function rellenarJaulas(num) {
 }
 
 function addSelect(arr,objetivo) {
+  for (let i = 0; i < arr.length; i++) {
+    let opcion = document.createElement("option")
+    opcion.setAttribute("value",i)
+    opcion.setAttribute("label",i+1)
+    opcion.setAttribute("class","jaulas")
+    objetivo.appendChild(opcion)
+  }
+  return objetivo ? true : false
+}
+
+function addRadio(arr,objetivo) {
   for (let i = 0; i < arr.length; i++) {
     let opcion = document.createElement("option")
     opcion.setAttribute("value",i)
@@ -48,6 +137,16 @@ function mostrar(texto,objetivo) {
   }
 }
 
+function soloNumeros(e) {
+  if (e.keyCode >= 48 && e.keyCode <= 57) {
+    // Números del 0 al 9
+    return true
+  } else {
+    e.preventDefault()
+    return false
+  }
+}
+
 class Jaula {
   constructor(especie) {
     this.especie = especie == undefined ? "" : especie
@@ -67,7 +166,7 @@ class Jaula {
   meterAnimales(nAnimales){
     let posible = this.especie != "" ? true : false
     if (posible) {
-      let total = this.numAnimales + nAnimales
+      let total = this.numAnimales + parseInt(nAnimales)
       posible = total <= this.maxAnimales ? true : false
       this.numAnimales = posible ? total : this.numAnimales
     }
